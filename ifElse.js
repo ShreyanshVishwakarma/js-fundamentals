3cc819e18244e77af1fc73296c6c83bcf2fd761c
ShreyanshVishwakarma <shreyanshvishwakarma@outlook.in>
2025-12-22 22:25:30 +0530

    if-else

---
+++ ifElse.js
@@ -0,0 +1,35 @@
+const user = "Alice";
+if (user) {
+  // Runs because "Alice" is a truthy string
+  console.log("User exists");
+}
+
+// really do i need to explain if statements ?
+
+//best practice : dont use nested ifs.
+
+//this is bad
+function login(user) {
+  if (user) {
+    if (user.password) {
+      // login logic...
+    }
+  }
+}
+
+//this is better
+function login2(user) {
+  if (!user) return; // Guard 1
+  if (!user.password) return; // Guard 2
+
+  // login logic (flat and clean)
+}
+
+// The ternary operator ....    condition ? true : false
+// BAD (Syntax Error)
+// const status = if (isActive) { "Online" } else { "Offline" };
+
+// GOOD
+const status = isActive ? "Online" : "Offline";
+
+// just remember don't nest ternary operators
